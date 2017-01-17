fc5b64076636fdd3175cfa611110e810230fea5f
Try-Polar <eb1033@york.ac.uk>
2017-01-17 17:36:06 +0000

    Added the remaining symbols
    fairly straightforward

--- src/gameDesigner/GameDesigner.java
+++ src/gameDesigner/GameDesigner.java
@@ -9,21 +9,41 @@ public class GameDesigner {
 	//Declare Non-Terminals
 	NonTerminalSymbol game;
 	NonTerminalSymbol levelBlock;
+	NonTerminalSymbol spriteBlock;
+	NonTerminalSymbol interactionBlock;
+	NonTerminalSymbol terminationBlock;
 	NonTerminalSymbol charMap;
 	NonTerminalSymbol spriteType;
+	NonTerminalSymbol spriteDef;
+	NonTerminalSymbol interactionDef;
+	NonTerminalSymbol terminationDef;
+	NonTerminalSymbol spriteSimple;
+	NonTerminalSymbol option;
+	NonTerminalSymbol eol;
+	
 			
 	//Declare Terminals	
-	TerminalSymbol eol;
+	TerminalSymbol newline;
 	TerminalSymbol indent;
 	TerminalSymbol identifier;
 	TerminalSymbol lambda;
+	TerminalSymbol greaterThan;
+	TerminalSymbol hash;
+	TerminalSymbol space;
+	TerminalSymbol equals;
 	//strings
 	TerminalSymbol game_class;
+	TerminalSymbol sprite_class;
+	TerminalSymbol interaction_method;
+	TerminalSymbol termination_class;
 	TerminalSymbol levelMapping;
 	TerminalSymbol variableChar;
-	TerminalSymbol greaterThan;
 	TerminalSymbol avatar;
 	TerminalSymbol wall;
+	TerminalSymbol spriteSet;
+	TerminalSymbol interactionSet;
+	TerminalSymbol terminationSet;
+	
 	
 	List<Symbol> gameSymbols = new LinkedList<Symbol>();
 	
@@ -38,38 +58,111 @@ public class GameDesigner {
 		//Declare Non-Terminals
 		game = new NonTerminalSymbol("game");
 		levelBlock = new NonTerminalSymbol("levelBlock");
+		spriteBlock = new NonTerminalSymbol("spriteBlock");
+		interactionBlock = new NonTerminalSymbol("interactionBlock");
+		terminationBlock = new NonTerminalSymbol("terminationBlock");
 		charMap = new NonTerminalSymbol("charMap");
 		spriteType = new NonTerminalSymbol("spriteType");
+		spriteDef = new NonTerminalSymbol("spriteDef");
+		interactionDef = new NonTerminalSymbol("interactionDef");
+		terminationDef = new NonTerminalSymbol("terminationdef");
+		spriteSimple = new NonTerminalSymbol("spriteSimple");
+		option = new NonTerminalSymbol("option");
+		eol = new NonTerminalSymbol("eol");
 		
 		//Declare Terminals		
-		eol = new TerminalSymbol("eol","\n");
+		newline = new TerminalSymbol("newline", "\n");
 		indent = new TerminalSymbol("indent","	");
 		identifier = new TerminalSymbol("identifier","IDENTIFIER");
 		lambda = new TerminalSymbol("lambda","LAMBDA");
+		greaterThan = new TerminalSymbol("greater than"," > ");
+		hash = new TerminalSymbol("hash", "#");
+		space = new TerminalSymbol("space", " ");
+		equals = new TerminalSymbol("equals", "=");
 		//strings
 		game_class = new TerminalSymbol("game_class","BasicGame"); //will not actually be terminal as it can expand to a number of things but this is simpler for now
+		sprite_class = new TerminalSymbol("sprite_class", ""); //will need to be passed as some sort of parameter or made into nonTerminal
+		interaction_method = new TerminalSymbol("interaction_method", ""); //will need to be passed as some sort of parameter or made into nonTerminal
+		termination_class = new TerminalSymbol("termination_class", "");
 		levelMapping = new TerminalSymbol("levelMapping","LevelMapping");
 		variableChar = new TerminalSymbol("char","CHAR"); //Just using "CHAR" as place holder for now
-		greaterThan = new TerminalSymbol("greater than"," > ");
 		avatar = new TerminalSymbol("avatar","avatar");
 		wall = new TerminalSymbol("wall","wall");
+		spriteSet = new TerminalSymbol("spriteSet","SpriteSet");
+		interactionSet = new TerminalSymbol("interactionSet","InteractionSet");
+		terminationSet = new TerminalSymbol("terminationSet","TerminationSet");
 		
 		//Add Children
+		//game
 		game.addChild(game_class);
 		game.addChild(eol);
 		game.addChild(indent);
 		game.addChild(levelBlock);
-		//g.sb
-		//g.ib
-		//g.tb
+		game.addChild(spriteBlock);
+		game.addChild(interactionBlock);
+		game.addChild(terminationBlock);
+		//level-block
 		levelBlock.addChild(levelMapping);
 		levelBlock.addChild(eol);
 		levelBlock.addChild(indent);
 		levelBlock.addChild(charMap);
+		levelBlock.addChild(newline);
+		//sprite-block
+		spriteBlock.addChild(spriteSet);
+		spriteBlock.addChild(eol);
+		spriteBlock.addChild(indent);
+		spriteBlock.addChild(charMap);
+		spriteBlock.addChild(newline);
+		//interaction-block
+		interactionBlock.addChild(interactionSet);
+		interactionBlock.addChild(eol);
+		interactionBlock.addChild(indent);
+		interactionBlock.addChild(interactionDef);
+		interactionBlock.addChild(eol);
+		//termination-block
+		terminationBlock.addChild(terminationSet);
+		terminationBlock.addChild(eol);
+		terminationBlock.addChild(indent);
+		terminationBlock.addChild(terminationDef);
+		terminationBlock.addChild(eol);
+		//char-map
 		charMap.addChild(variableChar);
 		charMap.addChild(greaterThan);
 		charMap.addChild(spriteType);
-		//charMap.addChild(lambda);
+		//sprite-def
+		spriteDef.addChild(spriteSimple);
+		//spriteDef.addChild(eol); //on hold for now until repeatable and optional sections are setup
+		//spriteDef.addChild(indent);
+		//spriteDef.addChild(spriteDef);
+		//spriteDef.addChild(eol);
+		//sprite-simple
+		spriteSimple.addChild(spriteType);
+		spriteSimple.addChild(greaterThan);
+		spriteSimple.addChild(sprite_class);
+		spriteSimple.addChild(space);
+		spriteSimple.addChild(option);
+		//interaction-def
+		interactionDef.addChild(spriteType);
+		interactionDef.addChild(spriteType);
+		interactionDef.addChild(greaterThan);
+		interactionDef.addChild(interaction_method);
+		interactionDef.addChild(space);
+		interactionDef.addChild(option);
+		//termination-def
+		terminationDef.addChild(termination_class);
+		terminationDef.addChild(space);
+		terminationDef.addChild(option);
+		//eol
+		//eol.addChild(space);
+		//eol.addChild(hash);
+		//eol.addChild(variableChar);
+		//eol.addChild(space);
+		eol.addChild(newline);
+		//option
+		option.addChild(identifier);
+		option.addChild(equals);
+		option.addChild(spriteType);
+		//option.addChild(evaluable); //requires OR functionality
 		spriteType.addChild(identifier);
 	}
 	
